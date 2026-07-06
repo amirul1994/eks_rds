387b3241a7ae11004958106c4f6595c62eec56be
amirul1994 <amirulbrinto90@gmail.com>
2026-07-06 11:57:44 +0600

    added eks cluster, eks cluster endpoint and oidc provider url in output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -30,3 +30,19 @@ output "frontend_repository_name" {
 output "vpc_id" {
     value = module.vpc.vpc_id
 }
+
+output "eks_cluster_name" {
+    value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_ca_data" {
+    value = module.eks.cluster_certificate_authority_data
+}
+
+output "oidc_provider_url" {
+    value = module.eks.oidc_provider_url
+}
